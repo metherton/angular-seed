971922e04586fb4d3a9d084c7fc3396c08f64b56
metherton <martinetherton63@googlemail.com>
2015-01-24 00:19:23 +0100

    added filter

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -17,10 +17,65 @@ describe('my app', function() {
     it('should load list of persons', function() {
         browser.get('http://localhost:8000/app/index.html#/persons');
         expect(element.all(by.repeater('(rowRenderIndex, row) in rowContainer.renderedRows track by $index')).count()).toEqual(10);
-        var rows = element.all(by.repeater('(rowRenderIndex, row) in rowContainer.renderedRows track by $index'));
-        _(rows).forEach(function(row) {
-           console.log(row);
+
+        var i = 1;
+//        element.all(by.repeater('(rowRenderIndex, row) in rowContainer.renderedRows track by $index')).filter(function(element) {
+//
+//            element.getText().then(console.log);
+//            console.log(i);
+//            i++;
+//            console.log(element.getText() === 'Etherton');
+//            return element.getText() === 'Etherton';
+//        }).each(function(e) {
+//            e.getText().then(console.log);
+//        });
+
+        element.all(by.repeater('(rowRenderIndex, row) in rowContainer.renderedRows track by $index')).filter(function(element) {
+
+            return element.getText().then(function(s) {
+               return s.indexOf('george') > -1;
+            });
+//            element.getText().then(function(s) {
+//                console.log('bla');
+//                console.log(s.indexOf('Etherton')  > -1);
+//            });
+//            element.getText().then(console.log);
+//            console.log(i);
+//            i++;
+//            return element.getText() === 'Etherton';
+
+//            function containsString(e) {
+//                return e.indexOf('samuel') > -1;
+//            }
+//
+//            return containsString(element.getText().then);
+
+//            element.getText().then(function(s) {
+//                return s.indexOf('samuel')  > -1;
+//            });
+
+//            return element.getText().then( function(s) {
+//                return s.indexOf('Etherton')  > -1)};
+//            })
+
+
+
+        }).each(function(e) {
+            e.getText().then(console.log);
+//            then(function(e) {
+//                console.log(e.getText());
+//            });
         });
+
+
+
+//        element.all(by.repeater('(rowRenderIndex, row) in rowContainer.renderedRows track by $index')).each(function(element) {
+//            element.getText().then(console.log);
+//        });
+//        _(rows).forEach(function(row) {
+//           console.log(row.getTagName());
+//        });
+
 
 
 
