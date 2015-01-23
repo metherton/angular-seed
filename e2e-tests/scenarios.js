57961bb3871970a9504b4d9324fa38d016b8cbc4
metherton <martinetherton63@googlemail.com>
2015-01-24 00:49:31 +0100

    add

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -19,65 +19,30 @@ describe('my app', function() {
         expect(element.all(by.repeater('(rowRenderIndex, row) in rowContainer.renderedRows track by $index')).count()).toEqual(10);
 
         var i = 1;
-//        element.all(by.repeater('(rowRenderIndex, row) in rowContainer.renderedRows track by $index')).filter(function(element) {
-//
-//            element.getText().then(console.log);
-//            console.log(i);
-//            i++;
-//            console.log(element.getText() === 'Etherton');
-//            return element.getText() === 'Etherton';
-//        }).each(function(e) {
-//            e.getText().then(console.log);
-//        });
 
         element.all(by.repeater('(rowRenderIndex, row) in rowContainer.renderedRows track by $index')).filter(function(element) {
-
             return element.getText().then(function(s) {
                return s.indexOf('george') > -1;
             });
-//            element.getText().then(function(s) {
-//                console.log('bla');
-//                console.log(s.indexOf('Etherton')  > -1);
+        }).first().click();
+
+
+//        element.all(by.repeater('(rowRenderIndex, row) in rowContainer.renderedRows track by $index')).filter(function(element) {
+//            return element.getText().then(function(s) {
+//                return s.indexOf('george') > -1;
 //            });
-//            element.getText().then(console.log);
-//            console.log(i);
-//            i++;
-//            return element.getText() === 'Etherton';
-
-//            function containsString(e) {
-//                return e.indexOf('samuel') > -1;
-//            }
-//
-//            return containsString(element.getText().then);
-
-//            element.getText().then(function(s) {
-//                return s.indexOf('samuel')  > -1;
+//        }).first.then(function(row) {
+//                row.click();
 //            });
 
-//            return element.getText().then( function(s) {
-//                return s.indexOf('Etherton')  > -1)};
-//            })
 
-
-
-        }).each(function(e) {
-            e.getText().then(console.log);
-//            then(function(e) {
-//                console.log(e.getText());
+//        element.all(by.repeater('(rowRenderIndex, row) in rowContainer.renderedRows track by $index')).filter(function(element) {
+//            return element.getText().then(function(s) {
+//                return s.indexOf('george') > -1;
 //            });
-        });
-
-
-
-//        element.all(by.repeater('(rowRenderIndex, row) in rowContainer.renderedRows track by $index')).each(function(element) {
-//            element.getText().then(console.log);
+//        }).each(function(e) {
+//            e.getText().then(console.log);
 //        });
-//        _(rows).forEach(function(row) {
-//           console.log(row.getTagName());
-//        });
-
-
-
 
     });
 
