ef0860bb1369a698887c1c8be567fb9c3337fccf
metherton <martinetherton63@googlemail.com>
2015-02-12 23:54:00 +0100

    cors

--- app/app.js
+++ app/app.js
@@ -313,6 +313,18 @@ onsApp.config(function($routeProvider, $provide, $httpProvider) {
     $routeProvider.otherwise({redirectTo: '/home'});
 
 
+    $httpProvider.defaults.withCredentials = true;
+    $httpProvider.defaults.headers.get = {
+    //    'Origin' : 'http://localhost:8000',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Origin': 'http://localhost:8000'
+    };
+
+    $httpProvider.defaults.headers.options = {
+        'Access-Control-Request-Headers': 'Access-Control-Allow-Credentials , Access-Control-Allow-Origin',
+        'Access-Control-Allow-Origin': 'http://localhost:8000'
+    };
+
 //    $provide.factory('myHttpInterceptor', function($q, $location) {
 //        return {
 //            // optional method
